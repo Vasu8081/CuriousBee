60eddd1fee552d68ac794c81f540c09c5b4ec357
Vasu8081 <vasudhanvarma@gmail.com>
2025-05-29 13:06:23 +0530

    fixes

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
-from app.db.database import get_db
-from app.db.models.users import Users
-from app.db.models.groups import Groups
+from app.routes import auth, users
 
 app = FastAPI()
 
-# app.include_router(auth.router, prefix="/auth")
-# app.include_router(users.router, prefix="/users")
-
-db = get_db()
-
-users = db.query(Users).all()
-
-group = users[0].group
-
-for user in group.users:
-    print(f"User: {user.name()}, Email: {user.email()}")
+app.include_router(auth.router, prefix="/auth")
+app.include_router(users.router, prefix="/users")

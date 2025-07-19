ed7c4aa4cde376532b75585068a43218f88353f0
Vasu8081 <vasudhanvarma@gmail.com>
2025-07-19 10:13:58 +0000

    added monitoring

--- curiousbytes-website/server/app/main.py
+++ curiousbytes-website/server/app/main.py
@@ -1,7 +1,9 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routers.youtube import router as youtube_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(youtube_router, prefix="/api/youtube")
